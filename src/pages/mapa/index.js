d3eabf24b10234bdfacb507513b04f2fb05b5472
carlapazjm <you@example.com>
2023-04-03 12:15:59 -0400

    install redux + maps k

--- src/pages/mapa/index.js
+++ src/pages/mapa/index.js
@@ -9,7 +9,7 @@ import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import ReactDOM from 'react-dom'
 
-const apiKey = 'AIzaSyC1XlvMbqs2CN_BWXFtk4BPwYWwD29cVww'
+const apiKey = 'AIzaSyAvrh6Dca2c-evpgQis3svXXAPUfRJsVEk'
 
 const UserLocationIcon = `
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" fill="none" class="css-i6dzq1">
