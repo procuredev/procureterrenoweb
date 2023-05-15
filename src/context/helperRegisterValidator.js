02b6f2fbf26e1bb1ba06976652caef7df639e551
dedwison <dedwison@gmail.com>
2023-05-15 09:30:28 -0400

    update helperRegisterValidator

--- src/context/helperRegisterValidator.js
+++ src/context/helperRegisterValidator.js
@@ -19,39 +19,40 @@ export function registerValidator(values) {
   const { name, rut, phone, email, company, role, plant, shift, contop, opshift } = values
 
   for (key in values) {
-    if (key != String) {
+    if (typeof values[key] !== 'string') {
       throw new Error(`El campo ${key} debe ser en formato texto`)
     }
   }
+
   if (!valName.test(name)) {
     throw new Error('El nombre debe contener solo letras y espacios en blanco.')
-  } else {
-    if (!valEmail.test(email)) {
-      throw new Error('El correo no cumple con el formato requerido')
-    } else {
-      if (!valPhone.test(phone)) {
-        throw new Error('El telefono solo recibe campos numericos')
-      } else {
-        if (!validateRut(rut)) {
-          throw new Error('El RUT no cumple con la validación del digito verificador')
-        } else {
-          if (company != 'Mel' || company != 'Procure') {
-            throw new Error('Deleccionar solo las Empresas sugeridas')
-          } else {
-            if (company === 'Procure' && !valRoleProcure.includes(role)) {
-              throw new Error('El Rol seleccionado no se encuentra entre los sugeridos para la la empresa Procure')
-            }
+  }
 
-            if (company === 'Mel' && !valRoleMel.includes(role)) {
-              throw new Error('El Rol seleccionado no se encuentra entre los sugeridos para la la empresa Mel')
-            } else {
-              if (!valPlant.includes(plant)) {
-                throw new Error('La Planta seleccionada no coinside con ninguna de las sugeridas')
-              }
-            }
-          }
-        }
-      }
-    }
+  if (!valEmail.test(email)) {
+    throw new Error('El correo no cumple con el formato requerido')
+  }
+
+  if (!valPhone.test(phone)) {
+    throw new Error('El telefono solo recibe campos numericos')
+  }
+
+  if (!validateRut(rut)) {
+    throw new Error('El RUT no cumple con la validación del digito verificador')
+  }
+
+  if (company !== 'Mel' && company !== 'Procure') {
+    throw new Error('Debe seleccionar solo las Empresas sugeridas')
+  }
+
+  if (company === 'Procure' && !valRoleProcure.includes(role)) {
+    throw new Error('El Rol seleccionado no se encuentra entre los sugeridos para la empresa Procure')
+  }
+
+  if (company === 'Mel' && !valRoleMel.includes(role)) {
+    throw new Error('El Rol seleccionado no se encuentra entre los sugeridos para la empresa Mel')
+  }
+
+  if (!valPlant.includes(plant)) {
+    throw new Error('La Planta seleccionada no coincide con ninguna de las sugeridas')
   }
 }
