5a965834ba1d1b98b29a4d3d69f1721d976ad3a6
carlapazjm <you@example.com>
2023-06-29 13:42:16 -0400

    locale-ES @ datagrid

--- src/views/table/data-grid/TableBasic.js
+++ src/views/table/data-grid/TableBasic.js
@@ -14,7 +14,7 @@ import { Typography, IconButton } from '@mui/material'
 import { Button } from '@mui/material'
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
-import { DataGrid } from '@mui/x-data-grid'
+import { DataGrid, 	esES } from '@mui/x-data-grid'
 import CardHeader from '@mui/material/CardHeader'
 import { DateRangePicker } from '@mui/lab'
 import { date } from 'yup/lib/locale'
@@ -268,6 +268,7 @@ const TableBasic = ({ rows, role, roleData }) => {
             end: xl,
             actions: roleData.canApprove
           }}
+          localeText={esES.components.MuiDataGrid.defaultProps.localeText}
         />
         <AlertDialog
           open={openAlert}
