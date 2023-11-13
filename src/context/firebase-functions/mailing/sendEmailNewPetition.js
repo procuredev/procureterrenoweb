af22e591f6f26c3a99c11160d4a7de1c27c19b8c
jbacuna91 <jorge.acuna@procure.cl>
2023-11-13 15:46:01 -0300

    message in email fix

--- src/context/firebase-functions/mailing/sendEmailNewPetition.js
+++ src/context/firebase-functions/mailing/sendEmailNewPetition.js
@@ -197,7 +197,7 @@ export const sendEmailNewPetition = async (user, values, reqId, reqNumber) => {
         arrayCC = arrayCC.concat(procureUsersEmail)
 
         let mcDescription = values.mcDescription ? values.mcDescription : ''
-        let specialMessage = `Dentro de los entregables solicitados se encuentra una "Memoria de Cálculo". A continuación puede encontrar el detalle indicado por el Solicitante:` + `<p></p>` + mcDescription + `<p></p>` + `Procure procederá a generar una presupuesto especial para el caso indicado.`
+        let specialMessage = `Dentro de los entregables solicitados se encuentra una "Memoria de Cálculo". A continuación puede encontrar el detalle indicado por el Solicitante:` + `<p></p>` + mcDescription + `<p></p>` + `Procure procederá a generar un presupuesto especial para el caso indicado.`
         lastMessage = specialMessage + ' ' + lastMessage
         emailHtml = getEmailTemplate(userName, mainMessage, requestNumber, title, engineering, otProcure, supervisor, start, end, plant, area, functionalLocation, contractOperator, petitioner, sapNumber, operationalType, machineDetention, jobType, deliverable, receiver, description, lastMessage)
 
