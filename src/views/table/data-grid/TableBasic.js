38489b2281e0ef65dd9659840f90c743590c7bbf
carlapazjm <you@example.com>
2023-11-28 17:24:50 -0300

    Create useColumnResizer hook and use in TableBasic.js

--- src/views/table/data-grid/TableBasic.js
+++ src/views/table/data-grid/TableBasic.js
@@ -5,6 +5,7 @@ import useMediaQuery from '@mui/material/useMediaQuery'
 import { useFirebase } from 'src/context/useFirebase'
 import dictionary from 'src/@core/components/dictionary/index'
 import { unixToDate } from 'src/@core/components/unixToDate'
+import useColumnResizer from 'src/@core/hooks/useColumnResizer'
 
 // ** MUI Imports
 import Fade from '@mui/material/Fade'
@@ -29,31 +30,9 @@ const TableBasic = ({ rows, role, roleData }) => {
   const [doc, setDoc] = useState('')
   const [approve, setApprove] = useState(true)
   const [loading, setLoading] = useState(true)
-  const [minWidths, setMinWidths] = useState({})
   const { updateDocs, authUser } = useFirebase()
 
-  const isResizing = useRef(-1);
-  const separatorRef = useRef(null);
-
-  useEffect(() => {
-    const columnWidthsString = localStorage.getItem('columnWidths');
-    const columnWidths = JSON.parse(columnWidthsString);
-    if (columnWidths) {
-      console.log('from local storage')
-      setMinWidths(columnWidths);
-    } else {
-      console.log('from default')
-      setMinWidths({...minWidths,
-        '.colIndex-1': {minWidth: '200px !important'},
-        '.colIndex-7': {minWidth: '70px !important'}
-      }
-        )
-    }
-  }, []);
-
-  useEffect(() => {
-    console.log(minWidths)
-  }, [minWidths])
+  const minWidths = useColumnResizer(loading)
 
   useEffect(() => {
     if (rows) {
@@ -189,108 +168,6 @@ const TableBasic = ({ rows, role, roleData }) => {
     }
   }, [rows])
 
-  const DEFAULT_MIN_WIDTH_CELL = 70;
-  const DEFAULT_MAX_WIDTH_CELL = 800;
-
-  const adjustWidthColumn = (index, width) => {
-    const minWidth = DEFAULT_MIN_WIDTH_CELL;
-    const maxWidth = DEFAULT_MAX_WIDTH_CELL;
-
-    let newWidth =
-      width > maxWidth ? maxWidth : width < minWidth ? minWidth : width;
-
-      // Selecciona todos los elementos de la columna
-      const columnElements = document.querySelectorAll(`[aria-colindex="${index}"]`);
-
-      // Itera sobre los elementos y ajusta su estilo
-      columnElements.forEach((element) => {
-        element.style.maxWidth = "none";
-        element.style.minWidth = "none";
-        element.style.width = newWidth + 'px';
-      });
-      console.log('adjustWidthColumn')
-      setMinWidths({...minWidths, [`.colIndex-${index}`]: {minWidth: `${newWidth}px !important`}})
-
-  }
-
-  const handleMiDivClick = (event) => {
-    separatorRef.current = event.srcElement.parentElement.parentNode.parentElement
-    const index = event.srcElement.parentElement.parentNode.parentElement.attributes[3] ? event.srcElement.parentElement.parentNode.parentElement.attributes[3].nodeValue : 0
-    isResizing.current = index;
-    setCursorDocument(true);
-  };
-
-  const handleMouseMove = (event) => {
-    if (isResizing.current >= 0) {
-      const width =  event.clientX - separatorRef.current.getBoundingClientRect().left;
-      adjustWidthColumn(isResizing.current, width);
-    }
-
-  };
-
-  const saveColumnWidthsLocalStorage = (columnWidths) => {
-    const columnWidthsString = JSON.stringify(columnWidths);
-    localStorage.setItem('columnWidths', columnWidthsString);
-  };
-
-  const getColumnWidths = () => {
-    console.log('getColumnWidths')
-    const columnWidths = {};
-    const columns = document.querySelectorAll('.MuiDataGrid-columnHeader');
-    columns.forEach((column) => {
-      const index = column.attributes[3].nodeValue;
-      const width = column.getBoundingClientRect().width;
-      columnWidths[`.colIndex-${index}`] = {minWidth: `${width}px !important`}
-    });
-
-    return columnWidths;
-  };
-
-  const handleMouseUp = (event) => {
-    if (isResizing.current >= 0) {
-      const columnWidths = getColumnWidths();
-      console.log('handleMouseUp')
-      setMinWidths(columnWidths);
-      saveColumnWidthsLocalStorage(columnWidths);
-    }
-    isResizing.current = -1;
-    separatorRef.current = null;
-    setCursorDocument(false);
-  };
-
-  const setCursorDocument = (isResizing) => {
-    document.body.style.cursor = isResizing ? "col-resize" : "auto";
-  };
-
-  useEffect(() => {
-    if (!loading) {
-      const miDivs = document.querySelectorAll('.MuiDataGrid-columnSeparator path')
-
-      if (miDivs) {
-        miDivs.forEach((div) => {
-          div.addEventListener('mousedown', handleMiDivClick);
-        });
-      }
-    }
-
-
-    return () => {
-      const miDivs = document.querySelectorAll('.MuiDataGrid-columnSeparator path');
-      miDivs.forEach((div) => {
-        div.removeEventListener('click', handleMiDivClick);
-      });}
-
-  }, [loading]);
-
-  useEffect(() => {
-    document.onmousemove = handleMouseMove
-    document.onmouseup = handleMouseUp
-
-    return () => {
-      document.onmousemove = null;
-      document.onmouseup = null;
-    };
-  }, []);
 
   const columns = [
     {
