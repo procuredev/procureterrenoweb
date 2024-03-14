97a952c37b456776db80d2390907b5b4ab1ff235
jbacuna91 <jorge.acuna@procure.cl>
2024-03-14 12:53:20 -0300

    Actualizar página sin ser redireccionado

--- src/context/useFirebase.js
+++ src/context/useFirebase.js
@@ -1,69 +1,76 @@
-import React, { createContext, useState, useEffect, useContext } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 
 // ** Firebase Imports
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
 import { app } from 'src/configs/firebase'
-import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth'
 
 // ** Crea contexto
 export const FirebaseContext = createContext()
 
 import {
+  createUser,
+  deleteCurrentUser,
   formatAuthUser,
   resetPassword,
-  updatePassword,
-  signInWithEmailAndPassword,
-  createUser,
   signAdminBack,
   signAdminFailure,
   signGoogle,
-  deleteCurrentUser
+  signInWithEmailAndPassword,
+  updatePassword
 } from 'src/context/firebase-functions/firebaseFunctions'
 
 import {
-  newDoc,
-  updateDocs,
-  updateUserPhone,
-  blockDayInDatabase,
-  generateBlueprint,
-  useBlueprints,
-  updateBlueprint,
+  addComment,
   addDescription,
+  blockDayInDatabase,
+  finishPetition,
+  generateBlueprint,
   generateBlueprintCodeClient,
   generateTransmittalCounter,
+  newDoc,
+  updateBlueprint,
+  updateDocs,
   updateSelectedDocuments,
-  addComment,
   updateUserData,
-  finishPetition
+  updateUserPhone,
+  useBlueprints
 } from 'src/context/firebase-functions/firestoreFunctions'
 
 import {
-  useEvents,
-  useSnapshot,
+  consultBlockDayInDB,
+  consultBluePrints,
+  consultDocs,
+  consultOT,
+  consultObjetives,
+  consultSAP,
+  consultUserEmailInDB,
+  fetchMelDeliverableType,
+  fetchMelDisciplines,
+  fetchPetitionById,
+  fetchPlaneProperties,
   getData,
   getDomainData,
   getUserData,
-  consultBlockDayInDB,
-  consultSAP,
-  consultUserEmailInDB,
-  consultDocs,
-  consultObjetives,
   getUsersWithSolicitudes,
-  fetchPetitionById,
-  fetchPlaneProperties,
-  fetchMelDisciplines,
-  fetchMelDeliverableType,
-  consultBluePrints,
+  subscribeToBlockDayChanges,
   subscribeToPetition,
-  consultOT,
   subscribeToUserProfileChanges,
-  subscribeToBlockDayChanges
+  useEvents,
+  useSnapshot
 } from 'src/context/firebase-functions/firestoreQuerys'
 
-import { uploadFilesToFirebaseStorage, updateUserProfile } from 'src/context/firebase-functions/storageFunctions'
+import { updateUserProfile, uploadFilesToFirebaseStorage } from 'src/context/firebase-functions/storageFunctions'
 
 const FirebaseContextProvider = props => {
   // ** Hooks
-  const [authUser, setAuthUser] = useState(null)
+  const [authUser, setAuthUser] = useState(() => {
+    if (typeof localStorage !== 'undefined') {
+      const storedUser = localStorage.getItem('user')
+      return storedUser ? JSON.parse(storedUser) : null
+    } else {
+      return null
+    }
+  })
   const [loading, setLoading] = useState(true)
   const [isCreatingProfile, setIsCreatingProfile] = useState(false)
   const [domainDictionary, setDomainDictionary] = useState({})
@@ -72,6 +79,31 @@ const FirebaseContextProvider = props => {
   // ** Variables
   const auth = getAuth(app)
 
+  function compareObjects(obj1, obj2) {
+    // Obtener las claves de cada objeto
+    const keys1 = Object.keys(obj1)
+    const keys2 = Object.keys(obj2)
+
+    // Verificar si el número de claves es el mismo
+    if (keys1.length !== keys2.length) {
+      return false
+    }
+
+    // Comparar propiedad por propiedad
+    for (let key of keys1) {
+      // Verificar si la propiedad existe en ambos objetos y si sus valores son iguales
+      if (!(key in obj2) || String(obj1[key]) !== String(obj2[key])) {
+        console.log('key: ' + key)
+        console.log('keyValue Objct 1: ' + obj1[key])
+        console.log('keyValue Objct 2: ' + obj2[key])
+        return false
+      }
+    }
+
+    return true
+  }
+
+  // Este useEffect manejará los datos del usuario conectado
   useEffect(() => {
     const auth = getAuth(app)
 
@@ -79,21 +111,34 @@ const FirebaseContextProvider = props => {
       if (!authState) {
         setAuthUser(null)
         setLoading(false)
-        setDomainDictionary(null)
-        setDomainRoles(null)
       } else {
         setLoading(true)
-        const formattedUser = await formatAuthUser(authState)
-        setAuthUser(formattedUser)
+        const databaseUserData = await formatAuthUser(authState)
+        setAuthUser(databaseUserData)
+        localStorage.setItem('user', JSON.stringify(databaseUserData))
         setLoading(false)
-        const dictionary = await getDomainData('dictionary')
-        setDomainDictionary(dictionary)
-        const roles = await getDomainData('roles')
-        setDomainRoles(roles)
       }
     })
 
     return () => unsubscribe()
+  }, [])
+
+  // useEffect para definir datos de tablas de dominio
+  useEffect(() => {
+    const defineDomain = async () => {
+      try {
+        const dictionary = await getDomainData('dictionary')
+        setDomainDictionary(dictionary)
+        const roles = await getDomainData('roles')
+        setDomainRoles(roles)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    if (authUser) {
+      defineDomain()
+    }
   }, [])
 
   const value = {
