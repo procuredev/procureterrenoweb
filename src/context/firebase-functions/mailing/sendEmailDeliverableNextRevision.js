5d3252c8671d32a4d0fed88566bb0277c39fbf63
jbacuna91 <jorge.acuna@procure.cl>
2024-11-21 17:21:56 -0300

    Se corrige texto

--- src/context/firebase-functions/mailing/sendEmailDeliverableNextRevision.js
+++ src/context/firebase-functions/mailing/sendEmailDeliverableNextRevision.js
@@ -307,7 +307,7 @@ export const sendEmailDeliverableNextRevision = async (user, petitionID, bluepri
         deliverableId: blueprint.id,
         emailType: 'DeliverableNextRevisor',
         message: {
-          subject: `Revsión de Entregable // OT ${petitionData.ot} // ${blueprint.id} // ${blueprint.clientCode}:`,
+          subject: `Revisión de Entregable // OT ${petitionData.ot} // ${blueprint.id} // ${blueprint.clientCode}:`,
           html: emailHtml
         }
       })
