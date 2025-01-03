03cff385cd720fc64faa267bd1884c4fefcb10fe
jbacuna91 <jorge.acuna@procure.cl>
2025-01-03 12:13:59 -0300

    OTRO TEST

--- src/pages/gabinete/index.js
+++ src/pages/gabinete/index.js
@@ -1,13 +1,12 @@
+// Hola
+
 // ** MUI Imports
 import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
 import DataGridGabinete from 'src/views/pages/gabinete/DataGridGabinete'
 
 // ** Styled Component
 
 // ** Demo Components Imports
-import FormLayoutSolicitud from 'src/views/pages/forms/form-layouts/FormLayoutsSolicitud'
-import LayoutSolicitudes from 'src/views/pages/solicitudes/DataGridSolicitudes'
 
 const Gabinete = () => {
   return (
