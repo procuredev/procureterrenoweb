70892d7fece2829e745a3cf5a878d01ba0cf9c51
jbacuna91 <jorge.acuna@procure.cl>
2025-01-03 12:36:43 -0300

    Test

--- src/pages/solicitudes/index.js
+++ src/pages/solicitudes/index.js
@@ -1,13 +1,10 @@
 // ** MUI Imports
 import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
 import DataGridSolicitudes from 'src/views/pages/solicitudes/DataGridSolicitudes'
 
 // ** Styled Component
 
 // ** Demo Components Imports
-import FormLayoutSolicitud from 'src/views/pages/forms/form-layouts/FormLayoutsSolicitud'
-import LayoutSolicitudes from 'src/views/pages/solicitudes/DataGridSolicitudes'
 
 const NuevaSolicitud = () => {
   return (
